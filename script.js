7115b1d2930857d2e52dba9232a981d589c65872
shiri castro <shiricastro3@gmail.com>
2017-07-22 19:28:51 +0300

    add component to the todo list

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ function listCtrlFun(){
         {description:"todo number 2", done:false},
         {description:"todo number 3", done:false}
     ];
-    console.log(this.todos);
- 
 }
+
 app.component('newTodo',{
     template:`<form class="mainTodo" ng-submit="newTodo.addTodo()">
                 <input class="mainInput" type="text"  ng-model="newTodo.newTodo" placeholder="What nedds to be done ?"/>
@@ -28,6 +27,19 @@ app.component('newTodo',{
     },
     controllerAs:"newTodo"    
 });
+
+app.component('todoList',{
+    template:`<div class="container">
+                <ul class="todoList">
+                    <todo-item data="list.data"></todo-item>
+                </ul>
+                <div class="footer">Tasks: <span>{{list.data.length}}</span></div>
+            </div>`,
+    bindings:{data:"="},
+    controller:function(){},
+    controllerAs:"list"     
+});
+
 app.component('todoItem',{
     template:`<li class="active" ng-repeat="todo in item.data" ng-class="{strikeLi:todo.done}">
                     <div class="view" >
