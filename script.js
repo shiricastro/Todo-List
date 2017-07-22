4a13166a15ced7c8f62a5efe52d489a5baf145a4
shiri castro <shiricastro3@gmail.com>
2017-07-22 17:35:58 +0300

    add some css

--- script.js
+++ script.js
@@ -21,29 +21,25 @@ app.component('todoList',{
     template:`<li class="active" ng-repeat="todo in item.data" ng-class="{strikeLi:todo.done}">
                     <div class="view" >
                         <input type="checkbox" ng-model="todo.done" />
-                        <span ng-class="{strike:todo.done}" ng-click="item.edit($event,$index)" >{{todo.description}}</span>
+                        <span ng-class="{strike:todo.done}" ng-click="item.edit($event)" >{{todo.description}}</span>
                         <button ng-click="item.deleteTodo($index)">✖</button>
                     </div>
                     <input class="edit" type="text" name="edit" ng-keyup="$event.keyCode == 13 ? item.save($event,$index) : null">
                 </li>`,
     bindings:{data:"="},
-    controller:function($element){
+    controller:function(){
         this.deleteTodo= function(index){
             this.data.splice(index,1);
         }; 
-        this.edit=function(e,index){
+        this.edit=function(e){
             var value = e.target.textContent;
-            angular.element($element[0].children[index]).removeClass('active');
-            angular.element($element[0].children[index].children[0]).css({"display":"none"});
-            var viewEdit =angular.element($element[0].children[index].children[1]);
-            viewEdit.css({"display":"block"});
-            viewEdit.val(value);            
+            angular.element((event.target).closest('li')).toggleClass('editing');
+            console.log(angular.element((event.target).closest('div.view')).next());
+            angular.element((event.target).closest('div.view')).next().val(value);
         };
         this.save = function(e,index){
             var newValue = e.target.value;
-            angular.element($element[0].children[index]).addClass('active');         
-            angular.element(e.target).css({"display":"none"});
-            angular.element($element[0].children[index].children[0]).css({"display":"flex"});
+            angular.element((event.target).closest('li')).toggleClass('editing');    
             this.data[index].description = newValue;
         };
     },
